8e8401fad3cd0840edb4199a0a367a24b0cc8543
DishaBafna <115555371+DishaBafna@users.noreply.github.com>
2022-11-07 10:22:27 -0500

    Java

---
+++ jquery demo/jquery.js
@@ -0,0 +1,19 @@
+// try playing with this ALERT - 
+// $(document).ready(function() {alert("[sup hello hello]"); })
+$(document).ready(function() {alert("[hello hello hello]"); })
+
+// JS files comment with two backslash lines 
+$(document).ready(function() {$("#ufo").draggable(); })
+
+$(document).ready(function() {$("#cat").draggable(); })
+
+$(document).ready(function() {$("#grapes").draggable(); })
+
+$(document).ready(function() {$("#diamond").draggable(); })
+
+$(document).ready(function() {$("#bananas").draggable(); })
+
+    
+$(document).ready(function() {$("#ufo").hover(function(){$("#earthling").css("color", "yellow");}); })
+$(document).ready(function()  {$("#ufo").hover(function(){$("#earthling").append("we come in peach");});})
+    
