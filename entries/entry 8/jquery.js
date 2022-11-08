59b54fea94bf908b608006e21567539281ca14c6
DishaBafna <115555371+DishaBafna@users.noreply.github.com>
2022-11-08 07:31:10 -0500

    Entry8

---
+++ entries/entry 8/jquery.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {$("#flour").draggable(); })
+
+$(document).ready(function() {$("#milk").draggable(); })
+
+$(document).ready(function() {$("#eggs").draggable(); })
+
+$(document).ready(function() {$("#butter").draggable(); })
+
+$(document).ready(function() {$("#sugar").draggable(); })
+
+$(document).ready(function() {$("#powder").draggable(); })
+
